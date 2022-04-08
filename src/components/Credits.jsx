cf1d306ad6f010eb58fbcef4d49e5bfef675abce
Thomas Thorin <thomas.thorin@gmail.com>
2022-04-08 08:11:52 +0200

    started on load screen

---
+++ src/components/Credits.jsx
@@ -0,0 +1,6 @@
+const Credits = () => (
+	<div>
+		<h1>Credits</h1>
+		<a href="https://www.flaticon.com/free-icons/pokeball" title="pokeball icons">Pokeball icons created by Darius Dan - Flaticon</a>
+	</div>
+)
